3a8922313965ac1a35b2e1aecc4dc1efe3012b5b
Ajibik <97358457+Ajibik@users.noreply.github.com>
2022-03-04 13:18:24 +0100

    Add files via upload

---
+++ AjibikeAbdulqayyum(CodeChallenge)/Number1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Number2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool check = true;
+            int maxcount = 0;
+            string first = "";
+            string h = "";
+            string m = "";
+            string second = "";
+            List<string> word = new List<string>(){"a", "ab", "abc", "d", "cd", "bcd", "abcd"};
+            for (var i = 0; i < word.Count; i++)
+            {
+                for (var j = 0; j < word.Count; j++)
+                {
+                    
+                    if (word[i].Length == word[j].Length && i != j)
+                    {
+
+                        h = $"{word[i]}{word[j]}";
+                        if (word[i].Length> maxcount)
+                        {
+                            maxcount = word[i].Length;
+                            foreach (var item in (h.Distinct()))
+                            {
+                                m+=item;
+                            }
+                            if(m.Length != h.Length)
+                            {
+                               
+                                break;
+                            }
+                            else
+                            {
+                                first = word[i];
+                                second = word[j];
+                            }
+                        } 
+                        m ="";                        
+                    }
+                } 
+                
+            }
+           
+                System.Console.WriteLine(first.Length*second.Length);
+            
+        }
+    }
+}
---
+++ AjibikeAbdulqayyum(CodeChallenge)/Number2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Number2
+{
+    class Program   
+    {
+        public static void Main(string[] args)
+        {
+            string T = "the day is sunny the the the sunny is is";
+            var splitT = T.Split(' ');
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            foreach(var elements in splitT)
+            {
+                if(elements != "")   
+                {
+                    string b = elements;
+                    if(dictionary.ContainsKey(b))
+                    {
+                        dictionary[b] = dictionary[b] + 1;
+                    }
+                    else
+                    {
+                        dictionary.Add(b, 1);
+                    }
+                }  
+            }
+            //var sort = dictionary.OrderBy(x => x.Value);
+            foreach (var item in dictionary)
+            {
+                System.Console.WriteLine($"{item.Key}\t{item.Value}");
+            }
+        }
+    }
+}
